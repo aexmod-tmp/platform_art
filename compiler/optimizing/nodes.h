c5ae7935684016b0235ab6713f46fe0b7ffd3171
Artem Serov <artem.serov@linaro.org>
2018-02-02 19:59:25 +0000

    ART: Rerun loop optimzizations after peeling
    This is a proof-of-concept which enables a second attempt of
    vectorizing of the loops which were peeled to eliminate
    invariant exists.
    
    The patch is only to show the concept and must be redisigned
    and optimized if considered to be picked up.
    
    Test: 530-checker-peel-unroll, 482-checker-loop-back-edge-use.
    Test: test-art-host, test-art-target, boot-to-gui.
    
    ~~~~THIS IS A WIP~~~~~
    Change-Id: Icc1a04d9794881daf38e0f23e138556409892031
    
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- compiler/optimizing/nodes.h
+++ compiler/optimizing/nodes.h
@@ -7099,6 +7099,10 @@ class HBoundType FINAL : public HExpression<1> {
     SetRawInputAt(0, input);
   }
 
+  bool CanBeMoved() const OVERRIDE { return true; }
+
+  bool InstructionDataEquals(const HInstruction* other) const OVERRIDE;
+
   bool IsClonable() const OVERRIDE { return true; }
 
   // {Get,Set}Upper* should only be used in reference type propagation.
