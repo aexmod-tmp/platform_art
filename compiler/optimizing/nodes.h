hopez13/art

X86: VarHandle.get() for reference type static fields.
This commit extends the VarHandle.get() implementation to work with
reference type fields, not only primitive types.

Test: art/test.py --host --32 -r -t 712-varhandle-invocations
Bug: 65872996
Change-Id: I7c0d3487e94ae00d120a5d1167b7a33ffabc6d20

compiler/optimizing/nodes.h
@@ -4514,6 +4514,7 @@ class HInvokePolymorphic final : public HInvoke {
  public:
   HInvokePolymorphic(ArenaAllocator* allocator,
                      uint32_t number_of_arguments,
+                     uint32_t number_of_other_arguments,
                      DataType::Type return_type,
                      uint32_t dex_pc,
                      uint32_t dex_method_index,
@@ -4524,7 +4525,7 @@ class HInvokePolymorphic final : public HInvoke {
       : HInvoke(kInvokePolymorphic,
                 allocator,
                 number_of_arguments,
-                /* number_of_other_inputs= */ 0u,
+                number_of_other_arguments,
                 return_type,
                 dex_pc,
                 dex_method_index,

aexmod-tmp/platform_art

Signed-off-by: DennySPb <dennyspb@gmail.com>

compiler/optimizing/nodes.h
@@ -4509,6 +4509,7 @@ class HInvokePolymorphic final : public HInvoke {
  public:
   HInvokePolymorphic(ArenaAllocator* allocator,
                      uint32_t number_of_arguments,
+                     uint32_t number_of_other_inputs,
                      DataType::Type return_type,
                      uint32_t dex_pc,
                      uint32_t dex_method_index,
@@ -4516,7 +4517,7 @@ class HInvokePolymorphic final : public HInvoke {
       : HInvoke(kInvokePolymorphic,
                 allocator,
                 number_of_arguments,
-                /* number_of_other_inputs= */ 0u,
+                number_of_other_inputs,
                 return_type,
                 dex_pc,
                 dex_method_index,