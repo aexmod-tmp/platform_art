hopez13/art

Disable support for proxy method in nterp.
They do not fall into the regular baseline JIT path (ie they cannot have
profiling info), so just bail on them for now.

Test: 044-proxy, wifi-tests
Bug: 112676029
Bug: 157658616
Bug: 160543640
Change-Id: I292d85f5d6bfd0edaad1d26e53f85f3780254fd7

runtime/art_method.h
@@ -525,11 +525,14 @@ class ArtMethod final {
   }
 
   ALWAYS_INLINE void SetProfilingInfo(ProfilingInfo* info) REQUIRES_SHARED(Locks::mutator_lock_) {
-    SetDataPtrSize(info, kRuntimePointerSize);
+    SetProfilingInfoPtrSize(info, kRuntimePointerSize);
   }
 
   ALWAYS_INLINE void SetProfilingInfoPtrSize(ProfilingInfo* info, PointerSize pointer_size)
       REQUIRES_SHARED(Locks::mutator_lock_) {
+    CHECK(!IsProxyMethod());
+    CHECK(!IsNative());
+    CHECK(IsInvokable());
     SetDataPtrSize(info, pointer_size);
   }
 

aexmod-tmp/platform_art

Signed-off-by: DennySPb <dennyspb@gmail.com>

runtime/art_method.h
@@ -523,11 +523,14 @@ class ArtMethod final {
   }
 
   ALWAYS_INLINE void SetProfilingInfo(ProfilingInfo* info) REQUIRES_SHARED(Locks::mutator_lock_) {
-    SetDataPtrSize(info, kRuntimePointerSize);
+    SetProfilingInfoPtrSize(info, kRuntimePointerSize);
   }
 
   ALWAYS_INLINE void SetProfilingInfoPtrSize(ProfilingInfo* info, PointerSize pointer_size)
       REQUIRES_SHARED(Locks::mutator_lock_) {
+    DCHECK(!IsProxyMethod());
+    DCHECK(!IsNative());
+    DCHECK(IsInvokable());
     SetDataPtrSize(info, pointer_size);
   }
 